78234af2eb9b0b99411347506fa6e8f4561c74f2
matlapo <mathieu.lapointe3@mail.mcgill.ca>
2018-06-15 20:01:18 -0400

    Rewrite of blinky with new Timer and GPIO drivers

--- os/kernel/src/kmain.rs
+++ os/kernel/src/kmain.rs
@@ -18,24 +18,15 @@ pub mod mutex;
 pub mod console;
 pub mod shell;
 
-const GPIO_BASE: usize = 0x3F000000 + 0x200000;
-
-const GPIO_FSEL1: *mut u32 = (GPIO_BASE + 0x04) as *mut u32;
-const GPIO_SET0: *mut u32 = (GPIO_BASE + 0x1C) as *mut u32;
-const GPIO_CLR0: *mut u32 = (GPIO_BASE + 0x28) as *mut u32;
-
 #[no_mangle]
 pub extern "C" fn kmain() {
 
-	unsafe {
-	    // STEP 1: Set GPIO Pin 16 as output.
-	    GPIO_FSEL1.write_volatile(0b001 << 18);
-	    // STEP 2: Continuously set and clear GPIO 16.
-	    loop {
-	        GPIO_SET0.write_volatile(1 << 16);
-	        pi::timer::spin_sleep_ms(1000);
-	        GPIO_CLR0.write_volatile(1 << 16);
-	        pi::timer::spin_sleep_ms(1000);
-	    }
+	//TEST #2
+	let mut pin16 = pi::gpio::Gpio::new(16 as u8).into_output();
+	loop {
+		pin16.set();
+		pi::timer::spin_sleep_ms(1000);
+		pin16.clear();
+		pi::timer::spin_sleep_ms(1000);
 	}
 }
